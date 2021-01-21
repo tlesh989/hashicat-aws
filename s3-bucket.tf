ca20677c9082fe0d3e44a0497e53d5585135354e
Tom Lesh <thomas.lesh@gmail.com>
2021-01-21 21:02:21 +0000

    changed s3 bucket name

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "my-s3-bucket"
+  bucket = "${var.prefix}-my-s3-bucket"
   acl    = "private"
 
   versioning = {
